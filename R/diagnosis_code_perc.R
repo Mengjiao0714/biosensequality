ed1a12427481039423ca8a1ec5d36d2cb7a12bc3
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-08-02 10:34:47 -0500

    Update diagnosis_code_perc.R

--- R/diagnosis_code_perc.R
+++ R/diagnosis_code_perc.R
@@ -1,7 +1,7 @@
 ##Frequency of occurrence in data - what is actually occurring in the data and
 ##how often each distinct entry is occurring and percentage of all visits each entry
 ##accounts for. 
-##Diagnosis_Code
+## For variable Diagnosis_Code
 
 diagnosis_code_perc<-function(data){
   number_visits=n_groups(group_by(data,C_BioSense_ID))
