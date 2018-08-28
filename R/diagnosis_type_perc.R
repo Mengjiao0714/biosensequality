7a52e2a5377a3da401798d9dbeba3f20393c8f19
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-08-28 14:07:23 -0500

    Update diagnosis_type_perc.R

--- R/diagnosis_type_perc.R
+++ R/diagnosis_type_perc.R
@@ -2,6 +2,7 @@
 ##Frequency of occurrence in data - what is actually occurring in the data and
 ##how often each distinct entry is occurring and percentage of all visits each entry
 ##accounts for. 
+## If the input is empty, say "Missing"
 ## Diagnosis_Type
 
 diagnosis_type_perc<-function(data){
@@ -9,7 +10,7 @@ diagnosis_type_perc<-function(data){
    
    Diagnosis_Type=data%>%
     select(C_BioSense_ID, Diagnosis_Type)%>%
-    mutate(Diagnosis_Type=ifelse(is.na(Diagnosis_Type),"NULL",Diagnosis_Type))%>%
+    mutate(Diagnosis_Type=ifelse(is.na(Diagnosis_Type),"Missing",Diagnosis_Type))%>%
     distinct(C_BioSense_ID,Diagnosis_Type,.keep_all=TRUE)%>%
     count(Diagnosis_Type)%>%
     transmute(Diagnosis_Type,count=n,percentage=round(100*n/number_visits,2))
