33458e3e9a49763e246e4e010c5e049dbc7bd51c
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-08-28 14:06:50 -0500

    Update diagnosis_code_perc.R

--- R/diagnosis_code_perc.R
+++ R/diagnosis_code_perc.R
@@ -8,7 +8,7 @@ diagnosis_code_perc<-function(data){
   
   Diagnosis_Code=data%>%
     select(C_BioSense_ID, Diagnosis_Code)%>%
-    mutate(Diagnosis_Code=ifelse(is.na(Diagnosis_Code),"NULL",Diagnosis_Code))%>%
+    mutate(Diagnosis_Code=ifelse(is.na(Diagnosis_Code),"Missing",Diagnosis_Code))%>%
     distinct(C_BioSense_ID,Diagnosis_Code,.keep_all=TRUE)%>%
     count(Diagnosis_Code)%>%
     transmute(Diagnosis_Code,count=n,percentage=round(100*n/number_visits,2))
