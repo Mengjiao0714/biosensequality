045d7846604c0d577d1c7ee30f508193907c18d9
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-08-28 14:08:52 -0500

    Update facility_type_description_perc.R

--- R/facility_type_description_perc.R
+++ R/facility_type_description_perc.R
@@ -1,6 +1,7 @@
 ##Frequency of occurrence in data - what is actually occurring in the data and
 ##how often each distinct entry is occurring and percentage of all visits each entry
 ##accounts for. 
+## If the input is empty, say "Missing"
 ##Facility_Type_Description
 
 facility_type_description_perc<-function(data){
@@ -9,7 +10,7 @@ facility_type_description_perc<-function(data){
   Facility_Type_Description=data%>%
     select(C_BioSense_ID, Facility_Type_Description)%>%
     mutate(Facility_Type_Description=toupper(Facility_Type_Description),
-           Facility_Type_Description=ifelse(is.na(Facility_Type_Description),"NULL",Facility_Type_Description))%>%
+           Facility_Type_Description=ifelse(is.na(Facility_Type_Description),"Missing",Facility_Type_Description))%>%
     distinct(C_BioSense_ID,Facility_Type_Description,.keep_all=TRUE)%>%
     count(Facility_Type_Description)%>%
     transmute(Facility_Type_Description,count=n,percentage=round(100*n/number_visits,2))
